10c866ee1674fd08036351dbc927318e3c5c0fe7
nmacawile <nmacawile@gmail.com>
2017-03-31 18:13:32 +0800

    include extensions

--- lib/web_guesser.rb
+++ lib/web_guesser.rb
@@ -1,29 +1,51 @@
 require "sinatra"
 require 'sinatra/reloader'
 
-#SECRET_NUMBER = rand(101)
 set :secret_number, rand(101)
+@@attempts = 3
 
-get("/") do
+get "/" do
 	guess = params["guess"]
-  	message = check_guess(guess)
-	erb(:index, :locals => { :number => settings.secret_number, :message => message })
+	cheat = params["cheat"]
+	difference = check_guess(guess)
+	color = change_color(difference)
+  	message = generate_message(difference, settings.secret_number)
+  	generate_number(difference)
+	erb(:index, :locals => { :number => settings.secret_number, :message => message, :color => color, :guesses => @@attempts, :cheat => cheat })
+end
+
+def generate_number(diff)
+	return if diff == nil
+	if @@attempts == 1 || diff == 0
+		settings.secret_number = rand(101)
+		@@attempts = 3
+	else
+		@@attempts -= 1
+	end
 end
 
 def check_guess(guess)
-	return "Guess the secret number. (1 to 100)" if guess.nil? || guess.match(/^\s*$/)
-	result = settings.secret_number - guess.to_i
+	return if guess.nil? || guess.match(/^\s*$/)
+	settings.secret_number - guess.to_i
+end
 
-	if result.between?(6, 100)
-		"WAY TOO LOW!"
-	elsif result.between?(1, 5)
-		"TOO LOW!"
-	elsif result == 0
-		"YOU GOT IT RIGHT! The Secret number is #{settings.secret_number}!!"
-	elsif result.between?(-5, -1)
-		"TOO HIGH!"
-	else
-		"WAY TOO HIGH!"
+def generate_message(diff, current_number)
+	return "Guess the secret number. (1 to 100)" if diff.nil?
+	return "YOU GOT IT RIGHT! The Secret number is #{current_number}!!" if diff == 0
+	return "You lost. The number was #{current_number}. A new number has been generated." if @@attempts == 1
+
+	if diff.between?(6, 100) then return "WAY TOO LOW! You have #{@@attempts - 1} attempts."
+	elsif diff.between?(1, 5) then return "TOO LOW! You have #{@@attempts - 1} attempts."	
+	elsif diff.between?(-5, -1) then return "TOO HIGH! You have #{@@attempts - 1} attempts."
+	else return "WAY TOO HIGH! You have #{@@attempts - 1} attempts."
 	end
+end
 
+def change_color(difference)
+	return "WHITE" if difference.nil?
+	return "green" if difference == 0
+	return "RED" if @@attempts == 1
+	difference = difference.abs	
+	offset = 100 - difference
+	"rgb(255, #{offset}, #{offset})"
 end
